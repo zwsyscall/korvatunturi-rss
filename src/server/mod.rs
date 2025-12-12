59054ee442c3caaf6b4f8ec860d0273e7b377e9b
zwsyscall <61552486+zwsyscall@users.noreply.github.com>
2025-12-12 21:24:02 +0200

    Add reply macros for more idiomatic tx write statements

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,3 +3,25 @@ mod server;
 
 pub use commands::ServerCommand;
 pub use server::start;
+
+#[macro_export]
+macro_rules! reply_err {
+    ($tx:expr, $($arg:tt)*) => {{
+        let msg = format!($($arg)*);
+        log::error!("{}", &msg);
+        if $tx.send(msg).is_err() {
+            error!("Error sending reply, channel closed");
+        }
+    }};
+}
+
+#[macro_export]
+macro_rules! reply_ok {
+    ($tx:expr, $($arg:tt)*) => {{
+        let msg = format!($($arg)*);
+        log::debug!("{}", &msg);
+        if $tx.send(msg).is_err() {
+            error!("Error sending reply, channel closed");
+        }
+    }};
+}
